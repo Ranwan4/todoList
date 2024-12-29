59faca7e34114003a7af48758e552b29f13db4f2
Ranwan4 <naoni2727@gmail.com>
2024-12-29 18:05:51 +0900

    first commit

---
+++ src/todoList/HellowWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package todoList;
+
+/**
+ * 
+ */
+public class HellowWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hellow World!");
+		
+		for(int i = 0; i < 5 ; i ++ ) {
+			System.out.println("---");
+			System.out.println(i);
+		}
+
+	}
+
+}
